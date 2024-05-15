5b2e5bac3f0fb6d8b33395778246e95fe26e0c2f
Naharro <pauhtto@gmail.com>
2024-05-15 17:28:18 +0200

    yes

--- src/main/java/cat/iesesteveterradas/PR32CreateMain.java
+++ src/main/java/cat/iesesteveterradas/PR32CreateMain.java
@@ -7,17 +7,17 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.parsers.DocumentBuilder;
+
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,78 +36,86 @@ public class PR32CreateMain {
             fileHandler.setFormatter(formatter);
 
             // Conexión a la base de datos MongoDB
-            var mongoClient = MongoClients.create("mongodb://root:example@localhost:27017");
-            MongoDatabase database = mongoClient.getDatabase("yourDatabaseName");
-            MongoCollection<Document> collection = database.getCollection("yourCollectionName");
+            String uri = "mongodb://elTeuUsuari:laTeuaContrasenya@localhost:27017/?authSource=admin";
+            try (MongoClient mongoClient = MongoClients.create(uri)) {
+                MongoDatabase database = mongoClient.getDatabase("yourDatabaseName");
+                MongoCollection<Document> collection = database.getCollection("yourCollectionName");
 
-            // Parsear el archivo XML
-            File file = new File("data/Posts.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            org.w3c.dom.Document doc = dBuilder.parse(file);
-            doc.getDocumentElement().normalize();
+                // Parsear el archivo XML
+                File file = new File("data/Posts.xml");
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                org.w3c.dom.Document doc = dBuilder.parse(file);
+                doc.getDocumentElement().normalize();
 
-            // Obtener la lista de nodos 'post'
-            NodeList nodeList = doc.getElementsByTagName("row");
+                // Obtener la lista de nodos 'post'
+                NodeList nodeList = doc.getElementsByTagName("row");
 
-            // Crear un nuevo documento XML para almacenar las preguntas seleccionadas
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            org.w3c.dom.Document resultDoc = builder.newDocument();
-            Element rootElement = resultDoc.createElement("posts");
-            resultDoc.appendChild(rootElement);
+                // Crear un nuevo documento XML para almacenar las preguntas seleccionadas
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                org.w3c.dom.Document resultDoc = builder.newDocument();
+                Element rootElement = resultDoc.createElement("posts");
+                resultDoc.appendChild(rootElement);
 
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
+                // Procesar solo las 20 primeras líneas
+                int count = 0;
+                for (int i = 0; i < nodeList.getLength() && count < 20; i++) {
+                    Node node = nodeList.item(i);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element) node;
 
-                    // Obtener los datos relevantes de cada pregunta
-                    String viewCountStr = element.getAttribute("ViewCount");
-                    int viewCount = 0; // Valor predeterminado en caso de que el atributo esté vacío
-                    if (!viewCountStr.isEmpty()) {
-                        viewCount = Integer.parseInt(viewCountStr);
-                    }
-                    String title = decodeEntities(element.getAttribute("Title"));
-                    String body = decodeEntities(element.getAttribute("Body"));
+                        // Obtener los datos relevantes de cada pregunta
+                        String viewCountStr = element.getAttribute("ViewCount");
+                        int viewCount = 0; // Valor predeterminado en caso de que el atributo esté vacío
+                        if (!viewCountStr.isEmpty()) {
+                            viewCount = Integer.parseInt(viewCountStr);
+                        }
+                        String title = decodeEntities(element.getAttribute("Title"));
+                        String body = decodeEntities(element.getAttribute("Body"));
 
-                    // Insertar en MongoDB si el post es una pregunta y cumple los requisitos
-                    if (element.getAttribute("PostTypeId").equals("1")) {
-                        Document pregunta = new Document("Id", element.getAttribute("Id"))
-                                .append("PostTypeId", element.getAttribute("PostTypeId"))
-                                .append("AcceptedanswerId", element.getAttribute("AcceptedanswerId"))
-                                .append("CreationDate", element.getAttribute("CreationDate"))
-                                .append("Score", element.getAttribute("Score"))
-                                .append("ViewCount", viewCount)
-                                .append("Body", body)
-                                .append("OwnerUserId", element.getAttribute("OwnerUserId"))
-                                .append("LastActivityDate", element.getAttribute("LastActivityDate"))
-                                .append("Title", title)
-                                .append("Tags", element.getAttribute("Tags"));
+                        // Insertar en MongoDB si el post es una pregunta y cumple los requisitos
+                        if (element.getAttribute("PostTypeId").equals("1")) {
+                            Document pregunta = new Document("Id", element.getAttribute("Id"))
+                                    .append("PostTypeId", element.getAttribute("PostTypeId"))
+                                    .append("AcceptedanswerId", element.getAttribute("AcceptedanswerId"))
+                                    .append("CreationDate", element.getAttribute("CreationDate"))
+                                    .append("Score", element.getAttribute("Score"))
+                                    .append("ViewCount", viewCount)
+                                    .append("Body", body)
+                                    .append("OwnerUserId", element.getAttribute("OwnerUserId"))
+                                    .append("LastActivityDate", element.getAttribute("LastActivityDate"))
+                                    .append("Title", title)
+                                    .append("Tags", element.getAttribute("Tags"));
 
-                        collection.insertOne(pregunta);
+                            collection.insertOne(pregunta);
 
-                        // Agregar la pregunta al documento XML de resultados
-                        Element postElement = resultDoc.createElement("post");
-                        postElement.setAttribute("Id", element.getAttribute("Id"));
-                        postElement.setAttribute("Title", title);
-                        postElement.setAttribute("Body", body);
-                        postElement.setAttribute("ViewCount", String.valueOf(viewCount));
-                        rootElement.appendChild(postElement);
+                            // Agregar la pregunta al documento XML de resultados
+                            Element postElement = resultDoc.createElement("post");
+                            postElement.setAttribute("Id", element.getAttribute("Id"));
+                            postElement.setAttribute("Title", title);
+                            postElement.setAttribute("Body", body);
+                            postElement.setAttribute("ViewCount", String.valueOf(viewCount));
+                            rootElement.appendChild(postElement);
+
+                            count++; // Incrementar el contador
+                        }
                     }
                 }
+
+                // Guardar el documento XML de resultados en un archivo
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource domSource = new DOMSource(resultDoc);
+                StreamResult streamResult = new StreamResult(new File("data/SelectedPosts.xml"));
+                transformer.transform(domSource, streamResult);
+
+                logger.log(Level.INFO, "Proceso completado. Resultados guardados en SelectedPosts.xml");
+                System.out.println("Proceso completado. Resultados guardados en SelectedPosts.xml");
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Error al conectar con MongoDB:", e);
+                e.printStackTrace();
             }
-
-            // Guardar el documento XML de resultados en un archivo
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource domSource = new DOMSource(resultDoc);
-            StreamResult streamResult = new StreamResult(new File("data/SelectedPosts.xml"));
-            transformer.transform(domSource, streamResult);
-
-            mongoClient.close(); // Cerrar la conexión a MongoDB
-            logger.log(Level.INFO, "Proceso completado. Resultados guardados en SelectedPosts.xml");
-            System.out.println("Proceso completado. Resultados guardados en SelectedPosts.xml");
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error en el proceso:", e);
             e.printStackTrace();
--- src/main/java/cat/iesesteveterradas/PR32QueryMain.java
+++ src/main/java/cat/iesesteveterradas/PR32QueryMain.java
@@ -1,5 +1,121 @@
 package cat.iesesteveterradas;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
 public class PR32QueryMain {
-    
+    private static final Logger logger = LoggerFactory.getLogger(PR32QueryMain.class);
+
+    public static void main(String[] args) {
+        try (var mongoClient = MongoClients.create("mongodb://elTeuUsuari:laTeuaContrasenya@localhost:27017")) {
+            MongoDatabase database = mongoClient.getDatabase("PR32");
+            MongoCollection<Document> collection = database.getCollection("Posts");
+            logger.info("Connected to MongoDB");
+
+            // Ejercicio 1: Calcular el promedio de ViewCount y consultar documentos con ViewCount mayor que el promedio
+            double averageViewCount = calculateAverageViewCount(collection);
+            FindIterable<Document> result1 = queryByViewCountGreaterThanAverage(collection, averageViewCount);
+
+            // Generar PDF para el Ejercicio 1
+            createPDF(result1, "informe1.pdf", doc -> doc.getString("title") + "; ViewCounts: " + doc.getInteger("viewCount"));
+
+            // Ejercicio 2: Buscar documentos que contengan ciertas palabras en el título
+            List<String> wordsToSearch = Arrays.asList("pug", "wig", "yak", "nap", "jig", "mug", "zap", "gag", "oaf", "elf", "hat", "D&D");
+            FindIterable<Document> result2 = queryByTitleContainingWords(collection, wordsToSearch);
+
+            // Generar PDF para el Ejercicio 2
+            createPDF(result2, "informe2.pdf", doc -> doc.getString("title"));
+
+        } catch (Exception e) {
+            logger.error("An error occurred: ", e);
+        }
+    }
+
+    private static double calculateAverageViewCount(MongoCollection<Document> collection) {
+        List<Document> allDocuments = new ArrayList<>();
+        try (MongoCursor<Document> allCursor = collection.find().iterator()) {
+            while (allCursor.hasNext()) {
+                allDocuments.add(allCursor.next());
+            }
+        }
+        double totalViewCount = 0;
+        for (Document doc : allDocuments) {
+            totalViewCount += doc.getInteger("viewCount", 0);
+        }
+        double averageViewCount = totalViewCount / allDocuments.size();
+        logger.info("Average viewCount: " + averageViewCount);
+        return averageViewCount;
+    }
+
+    private static FindIterable<Document> queryByViewCountGreaterThanAverage(MongoCollection<Document> collection, double averageViewCount) {
+        Document query = new Document("viewCount", new Document("$gt", averageViewCount));
+        FindIterable<Document> result = collection.find(query);
+        logger.info("Query 1 done");
+        return result;
+    }
+
+    private static FindIterable<Document> queryByTitleContainingWords(MongoCollection<Document> collection, List<String> wordsToSearch) {
+        String regexPattern = String.join("|", wordsToSearch);
+        Document query = new Document("title", new Document("$regex", regexPattern));
+        FindIterable<Document> result = collection.find(query);
+        logger.info("Query 2 done");
+        return result;
+    }
+
+    private static void createPDF(FindIterable<Document> documents, String fileName, TextExtractor textExtractor) {
+        try (PDDocument doc = new PDDocument()) {
+            PDPage page = new PDPage();
+            doc.addPage(page);
+
+            // Inicializar contenido del PDF
+            try (PDPageContentStream contents = new PDPageContentStream(doc, page)) {
+                contents.beginText();
+                contents.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 12);
+                contents.setLeading(14.5f);
+                contents.newLineAtOffset(25, 750);
+
+                // Agregar contenido al PDF
+                for (Document docMongoDB : documents) {
+                    String text = textExtractor.extractText(docMongoDB);
+                    if (text != null && !text.isEmpty()) {
+                        logger.info("Adding text to PDF: " + text);
+                        contents.showText(text);
+                        contents.newLine();
+                    } else {
+                        logger.warn("Empty or null text found in document: " + docMongoDB.toJson());
+                    }
+                }
+                contents.endText();
+            }
+
+            // Guardar el PDF
+            String outputPath = System.getProperty("user.dir") + "/data/out/" + fileName;
+            doc.save(outputPath);
+            logger.info("PDF created: " + outputPath);
+        } catch (IOException e) {
+            logger.error("Error creating PDF: ", e);
+        }
+    }
+
+    @FunctionalInterface
+    interface TextExtractor {
+        String extractText(Document document);
+    }
 }
